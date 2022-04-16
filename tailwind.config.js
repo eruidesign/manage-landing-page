b606483977758a60cae2429a338a2bb5add46f87
eRui Design <50480764+eruidesign@users.noreply.github.com>
2022-04-16 23:52:22 +0800

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,16 @@
 module.exports = {
   content: ['./*.html'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        BrightRed: 'hsl(12, 88%, 59%)', 
+        DarkBlue: 'hsl(228, 39%, 23%)',
+        DarkGrayishBlue: 'hsl(227, 12%, 61%)', 
+        VeryDarkBlue: 'hsl(233, 12%, 13%)', 
+        VeryPaleRed: 'hsl(13, 100%, 96%)', 
+        VeryLightGray: 'hsl(0, 0%, 98%)'
+      }
+    },
   },
   plugins: [],
 }
